e91f74648bd760e6d2a419440e1cefb8de91691a
DanielSP99 <daniel.sanchez@ingenierosmafur.com>
2022-08-30 16:38:45 -0500

    Initial Commit

---
+++ music_clone/music_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  music_cloneApp.swift
+//  music_clone
+//
+//  Created by Daniel Sanchez Peraza on 30/08/22.
+//
+
+import SwiftUI
+
+@main
+struct music_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
